3a35b3b7922f16f9f645ae40cb34da8c880e1c69
angs0003 <140598238+angs0003@users.noreply.github.com>
2024-01-10 04:13:02 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+connect = sqlite3.connect('data.db')
+
+connect.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, username TEXT, password TEXT)')
+
+connect.close()
